c160859e8facca850166b658e4bc95a10baa5865
DrMetr <dashagamlo@mail.ru>
2025-03-10 18:37:28 +0300

    Major commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const email = document.querySelector('#email');
+email.addEventListener('input', onInput);
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+function onInput() {
+    if (isEmailValid(input.value)) {
+      input.style.borderColor = 'green';
+    } else {
+      input.style.borderColor = 'red';
+    }
+  }
+  
+  input.addEventListener('input', onInput);
+  
+  function isEmailValid(value) {
+  return EMAIL_REGEXP.test(value);
+  }
